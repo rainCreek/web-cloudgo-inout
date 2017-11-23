a91e9a12be3fc0e6d8257c86eb26394ac40dda07
RaoYuxi <805459700@qq.com>
2017-11-23 23:07:53 +0800

    fix some mistakes

--- service/handler.go
+++ service/handler.go
@@ -38,11 +38,22 @@ func fillinHandler(formatter *render.Render) http.HandlerFunc {
   return func(w http.ResponseWriter, req *http.Request) {
     if req.URL.Path == "/fillin" {
       t, err := template.ParseFiles("./templates/form.html")
+
+      req.ParseForm()
+      na := req.FormValue("name")
+      ag := req.FormValue("age")
+      gd := req.FormValue("gender")
+      formatter.HTML(w, http.StatusOK, "table", struct {
+        Name      string `json:"name"`
+        Age        string `json:"age"`
+        Gender      string `json:"gender"`
+      } {Name: na, Age: ag, Gender: gd})
       if err != nil {
         log.Println(err)
       }
       t.Execute(w, nil)  
     }     
   }
+
 }
 
